7f8f58e896c0b0efe12a04f4d5592164fb81785c
cyrils <nigthguarder@gmail.com>
2023-12-10 20:23:33 +0100

    create server only

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-// app.js
+// requirements
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
-const { router } = require('./src/auth/router');
+const AuthRoute = require('./src/auth/auth');
 
 //Local Variables
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(bodyParser.json());
-app.use(("/api/v1/auth"),router)
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
 
-module.exports = app;
